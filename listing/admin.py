0d76f56e4dcadd5b5eab7482b24ab1857bcd1a26
Mathenge-Alex <arxmaina@gmail.com>
2022-06-22 13:54:24 +0300

    Feat: Register admin models

---
+++ listing/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from listing.models import *
+# Register your models here.
+
+admin.site.register(Business)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(NeighborHood)
