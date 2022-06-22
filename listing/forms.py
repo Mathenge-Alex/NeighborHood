bf289530ec8717f1fcb602bfbe55506b31ab4923
Mathenge-Alex <arxmaina@gmail.com>
2022-06-22 13:52:40 +0300

    Feat: Create Hood form

---
+++ listing/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Business,Profile,NeighborHood,Post,Business,Comment
+
+    # Forms
+        
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighborHood
+        exclude= ['occupants']
+
