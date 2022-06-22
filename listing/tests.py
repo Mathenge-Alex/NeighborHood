c00559d6ef167c49c70c39dcd32a10a2e87dda83
Mathenge-Alex <arxmaina@gmail.com>
2022-06-22 14:04:53 +0300

    Feat: add Profile Test class

--- listing/tests.py
+++ listing/tests.py
@@ -28,3 +28,23 @@ class BusinessTestCase(TestCase):
 
 
 
+class profileTestCLass(TestCase):
+    '''
+    setup self instance of profile
+    '''
+    def setUp(self):
+        self.prof = Profile(Bio='We are in this together')
+    
+    ''' 
+    test instance of profile
+    '''
+    def test_instance(self):
+        self.assertTrue(isinstance(self.prof,Profile))
+
+    def test_save_profile(self):
+        self.prof.save_profile()
+        bio = Profile.objects.all()
+        self.assertTrue(len(bio)>0)
+
+
+
