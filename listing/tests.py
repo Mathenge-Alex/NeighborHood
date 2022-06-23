0e24a3300b9a5eec88be4c92bbad957edc40f534
Mathenge-Alex <arxmaina@gmail.com>
2022-06-23 06:01:25 +0300

    Feat: Add Profile test Class

--- listing/tests.py
+++ listing/tests.py
@@ -6,28 +6,6 @@ from .models import Comment, Business,Profile,Post
 # Create your tests here.
 
 
-class BusinessTestCase(TestCase):
-    '''
-    setup
-    '''
-    def setUp(self):
-        
-        self.business = Business(name='jirani',image='jirani.png',pub_date='21,Jun,2022',user='1',NeighborHood='1')
-    '''
-    test instance of business
-    '''
-    def test_instance(self):
-        self.assertTrue(isinstance(self.business,Business))
-        '''
-        test for save instance of business
-        '''
-    def test_save_business(self):
-        self.business.save_business()
-        name = Business.objects.all()
-        self.assertTrue(len(name)>0)
-
-
-
 class profileTestCLass(TestCase):
     '''
     setup self instance of profile
@@ -45,6 +23,4 @@ class profileTestCLass(TestCase):
         self.prof.save_profile()
         bio = Profile.objects.all()
         self.assertTrue(len(bio)>0)
-
-
 
