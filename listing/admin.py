da76f9c4eae40b7f4a1913da88add498d9bf2c2e
Mathenge-Alex <arxmaina@gmail.com>
2022-06-23 05:35:25 +0300

    Config: Add admin site models

--- listing/admin.py
+++ listing/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from listing.models import *
 # Register your models here.
 
-admin.site.register(Business)
 admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Business)
 admin.site.register(NeighborHood)
+admin.site.register(Comment)
+admin.site.register(Post)
