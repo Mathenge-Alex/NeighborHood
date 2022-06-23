a8ae9d6643ed3f3e0730479b7f839a4241f79dba
Mathenge-Alex <arxmaina@gmail.com>
2022-06-23 06:01:43 +0300

    Feat: Add Business test Class

--- listing/tests.py
+++ listing/tests.py
@@ -24,3 +24,23 @@ class profileTestCLass(TestCase):
         bio = Profile.objects.all()
         self.assertTrue(len(bio)>0)
 
+
+class BusinessTestCase(TestCase):
+    '''
+    setup
+    '''
+    def setUp(self):
+        
+        self.business = Business(name='jirani',image='jirani.png',pub_date='21,Jun,2022',user='1',NeighborHood='1')
+    '''
+    test instance of business
+    '''
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business,Business))
+        '''
+        test for save instance of business
+        '''
+    def test_save_business(self):
+        self.business.save_business()
+        name = Business.objects.all()
+        self.assertTrue(len(name)>0)
