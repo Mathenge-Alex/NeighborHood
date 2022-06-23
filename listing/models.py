130bd5761455011cce3ea541958ab8349e110052
Mathenge-Alex <arxmaina@gmail.com>
2022-06-23 05:46:58 +0300

    Feat: Add Post model class

--- listing/models.py
+++ listing/models.py
@@ -94,3 +94,26 @@ class Business(models.Model):
     def search(cls,search_term):
         biz = cls.objects.filter(name__icontains=search_term)
         return biz
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=50,blank=True)
+    image = CloudinaryField("image")
+    description = models.TextField(max_length = 50,null = True)
+    user = models.ForeignKey(User, null = True,related_name='post',on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+    neighborHood = models.ForeignKey(NeighborHood, null = True,related_name='posts',on_delete=models.CASCADE)
+    
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+    def __str__(self):
+        return self.name
+    @classmethod
+    def get_hood_posts(cls,id):
+        posts = Post.objects.filter(id = id)
+        return posts
+
+
