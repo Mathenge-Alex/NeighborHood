6fa25cd2a0dbb6d8af43eae68f85b5392cbb3da4
Mathenge-Alex <arxmaina@gmail.com>
2022-06-23 05:45:39 +0300

    Feat: Add hood model

--- listing/models.py
+++ listing/models.py
@@ -44,3 +44,22 @@ class Profile(models.Model):
         userprof = Profile.objects.filter(user__username__icontains = name)
         return userprof
 
+
+class NeighborHood(models.Model):
+    view =  CloudinaryField("image")
+    name = models.CharField(max_length=50,blank=True)
+    location = models.CharField(max_length = 50,null = True)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+    occupants = models.ForeignKey(User, null = True,related_name='business',on_delete=models.CASCADE)
+    
+    def save_hood(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def get_all_hoods(cls):
+        hood = NeighborHood.objects.all()
+        return hood
+
